3c525276f84b47df548e9f042e381a2c0b5b9dd1
badr__ihaddouhen <badrmtrl@gmail.com>
2021-01-17 02:59:08 -0500

    networking_java_lab

---
+++ src/Networking/URLConnDemo.java
@@ -0,0 +1,33 @@
+package Networking;
+
+import java.net.*;
+import java.io.*;
+
+public class URLConnDemo {
+
+   public static void main(String [] args) {
+      try {
+         URL url = new URL("https://www.google.ca");
+         URLConnection urlConnection = url.openConnection();
+         HttpURLConnection connection = null;
+         if(urlConnection instanceof HttpURLConnection) {
+            connection = (HttpURLConnection) urlConnection;
+         }else {
+            System.out.println("Please enter an HTTP URL.");
+            return;
+         }
+         
+         BufferedReader in = new BufferedReader(
+            new InputStreamReader(connection.getInputStream()));
+         String urlString = "";
+         String current;
+         
+         while((current = in.readLine()) != null) {
+            urlString += current;
+         }
+         System.out.println(urlString);
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+   }
+}
